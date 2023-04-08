ae1427da188c5867ec0c73f503e65ab813db7a03
Hyunwoong Chang <41578577+hwchang1201@users.noreply.github.com>
2023-04-07 19:24:40 -0500

    Delete R/competing_models directory

--- R/competing_models/EqVarDAG/startupEqVarDAG.R
+++
@@ -1,8 +0,0 @@
-source("R/DAG_from_Ordering.R")
-source("R/EqVarDAG_BU.R")
-source("R/EqVarDAG_HD_CLIME.R")
-source("R/EqVarDAG_HD_TD.R")
-source("R/EqVarDAG_TD.R")
-source("R/randGGM.R")
-source("R/vselect.R")
-
--- R/competing_models/GDS/startups/startupGES.R
+++
@@ -1,6 +0,0 @@
-# Copyright (c) 2010 - 2013  Jonas Peters  [peters@stat.math.ethz.ch]
-# All rights reserved.  See the file COPYING for license terms. 
-
-library(pcalg, lib.loc = "~/R/library") 
-source("../inferDAG/gesWrap.R")
-
